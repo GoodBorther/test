bf7651ea1c34fac048509ac051a2c2b2658732bc
mcj <mcj5171198@163.com>
2020-09-08 20:18:33 +0800

    changed test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-print(112233)
-
+def move(n,a,b,c):
+    if n == 1:
+        print("a" "==>" "c")
+    else:
+        print("a" "==>" "b")
+        move(n-1,a,b,c):
+move(3,"A","B","C")
