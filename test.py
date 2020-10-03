04f2b3ba7e21ef8e45e125aa5e6379bdcf7e9494
mcj <mcj5171198@163.com>
2020-10-03 16:59:02 +0800

    taluopaishiyan

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 def move(n,a,b,c):
-    if n == 1:
-        print("a" "==>" "c")
+    if n==1:
+        print (a,"-->",c)
     else:
-        print("a" "==>" "b")
-        move(n-1,a,b,c):
-move(3,"A","B","C")
-c
+        move(n-1,a,c,b)
+        print (a,'-->',c)
+        move (n-1,b,a,c)
+print (move(3,"A","B","C"))
