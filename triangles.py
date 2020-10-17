74aa26ff461553f3210da3f374b4eedf036bd766
mcj <mcj5171198@163.com>
2020-10-17 17:31:23 +0800

    已完成

--- triangles.py
+++ triangles.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 #-*- conding: utf-8 -*-
-def triangles():
-    conunt=0
-    
+def triangles(m=11):
+    num = [1]
+    while m > 0:
+        yield num
+        for  i in range(1, len(num)):
+            num[i] = pre[i] + pre[i - 1]
+        num.append(1)
+        pre = num[:]
+        m = m - 1
+for i in triangles():
+    print (i)
