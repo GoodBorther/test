40d52171374b4ae147b77e2dd243416e162f97da
mcj <mcj5171198@163.com>
2020-10-21 19:09:35 +0800

    第七天python作业

--- day07/day07.py
+++ day07/day07.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 #-*- conding: utf-8 -*-
 class Student(object):
+    count = 0
     def __init__(self,name,score):
         self.__name = name
         self.__score = score
+        Student.count += 1
     def print_score(self):
         print ('%s : %s' % (self.__name,self.__score))
     def print_grade(self):
@@ -12,4 +14,5 @@ class Student(object):
         else:
             return 'this is bad'
 student = Student('mcj',10)
-print (student.print_grade())
+qaq = Student('wangwu',20)
+print (Student.count)
